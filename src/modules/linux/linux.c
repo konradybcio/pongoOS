2fc8346564043c4b911c014ab8ee53c0e48efe99
Markuss Broks <markuss.broks@gmail.com>
2022-06-01 20:31:50 +0300

    messy but works

--- src/modules/linux/linux.c
+++ src/modules/linux/linux.c
@@ -40,127 +40,141 @@ char gLinuxCmdLine[LINUX_CMDLINE_SIZE] = {0};
 
 void linux_dtree_init(void)
 {
-    // TODO: TONS of error handling
-    char compatible_apple[64];
-    char *arm_io_type = dt_get_prop("arm-io", "device_type", NULL);
-    char soc_name[64];
-    char fdt_nodename[64];
-    strncpy(soc_name, arm_io_type, 6);
-    soc_name[5] = 0;
-    compatible_apple[0] = 0;
-    strcat(compatible_apple, "apple,");
-    strcat(compatible_apple, soc_name);
-    if (fdt_initialized)
-        free(fdt);
-    fdt = malloc(LINUX_DTREE_SIZE);
-    fdt_create_empty_tree(fdt, LINUX_DTREE_SIZE);
+//     // TODO: TONS of error handling
+//     char compatible_apple[64];
+//     char *arm_io_type = dt_get_prop("arm-io", "device_type", NULL);
+//     char soc_name[64];
+//     char fdt_nodename[64];
+//     strncpy(soc_name, arm_io_type, 6);
+//     soc_name[5] = 0;
+//     compatible_apple[0] = 0;
+//     strcat(compatible_apple, "apple,");
+//     strcat(compatible_apple, soc_name);
+//     if (fdt_initialized)
+//         free(fdt);
+//     fdt = malloc(LINUX_DTREE_SIZE);
+//     fdt_create_empty_tree(fdt, LINUX_DTREE_SIZE);
 
-    int node = 0, node1 = 0;
-    fdt_appendprop_string(fdt, node, "compatible", compatible_apple);
-    uint32_t size;
-    void *prop = dt_prop(gDeviceTree, "product-name", &size);
-    char name[20];
-    strncpy(name, prop, size);
-    name[size] = 0;
-    fdt_appendprop_string(fdt, node, "name", name);
-    fdt_appendprop_cell(fdt, 0, "#address-cells", 0x2);
-    fdt_appendprop_cell(fdt, 0, "#size-cells", 0x2);
-    fdt_appendprop_cell(fdt, 0, "interrupt-parent", 0x1);
+//     int node = 0, node1 = 0;
+//     fdt_appendprop_string(fdt, node, "compatible", compatible_apple);
+//     uint32_t size;
+//     void *prop = dt_prop(gDeviceTree, "product-name", &size);
+//     char name[20];
+//     strncpy(name, prop, size);
+//     name[size] = 0;
+//     fdt_appendprop_string(fdt, node, "name", name);
+//     fdt_appendprop_cell(fdt, 0, "#address-cells", 0x2);
+//     fdt_appendprop_cell(fdt, 0, "#size-cells", 0x2);
+//     fdt_appendprop_cell(fdt, 0, "interrupt-parent", 0x1);
 
-    /* Alias */
-    node = fdt_add_subnode(fdt, 0, "/aliases");
-    char serials[64];
-    siprintf(serials, "/soc/serial@%llx", ((uint64_t)dt_get_u32_prop("uart0", "reg")) + gIOBase);
-    fdt_appendprop_string(fdt, node, "serial0", serials);
+//     /* Alias */
+//     node = fdt_add_subnode(fdt, 0, "/aliases");
+//     char serials[64];
+//     siprintf(serials, "/soc/serial@%llx", ((uint64_t)dt_get_u32_prop("uart0", "reg")) + gIOBase);
 
-    /* CPU */
-    node = fdt_add_subnode(fdt, 0, "/cpus");
-    fdt_appendprop_string(fdt, node, "name", "cpus");
-    fdt_appendprop_cell(fdt, node, "#address-cells", 0x1);
-    fdt_appendprop_cell(fdt, node, "#size-cells", 0x0);
+//     fdt_appendprop_string(fdt, node, "serial0", serials);
 
-    node1 = fdt_add_subnode(fdt, node, "/cpu@0");
-    fdt_appendprop_string(fdt, node1, "device_type", "cpu");
-    fdt_appendprop_string(fdt, node1, "compatible", "hx,v1");
-    fdt_appendprop_cell(fdt, node1, "reg", 0);
+//     /* CPU */
+//     node = fdt_add_subnode(fdt, 0, "/cpus");
+//     fdt_appendprop_string(fdt, node, "name", "cpus");
+//     fdt_appendprop_cell(fdt, node, "#address-cells", 0x1);
+//     fdt_appendprop_cell(fdt, node, "#size-cells", 0x0);
 
-    node1 = fdt_add_subnode(fdt, node, "/cpu@1");
-    fdt_appendprop_string(fdt, node1, "device_type", "cpu");
-    fdt_appendprop_string(fdt, node1, "compatible", "hx,v1");
-    fdt_appendprop_cell(fdt, node1, "reg", 1);
+//     node1 = fdt_add_subnode(fdt, node, "/cpu@0");
+//     fdt_appendprop_string(fdt, node1, "device_type", "cpu");
+//     fdt_appendprop_string(fdt, node1, "compatible", "hx,v1");
+//     fdt_appendprop_cell(fdt, node1, "reg", 0);
 
-    /* refclk */
-    node = fdt_add_subnode(fdt, 0, "/refclk24mhz");
-    fdt_appendprop_string(fdt, node, "compatible", "fixed-clock");
-    fdt_appendprop_string(fdt, node, "clock-output-names", "refclk24mhz");
-    fdt_appendprop_cell(fdt, node, "#clock-cells", 0x00000000);
-    fdt_appendprop_cell(fdt, node, "clock-frequency", 0x016e3600);
-    fdt_appendprop_cell(fdt, node, "phandle", 2);
+//     node1 = fdt_add_subnode(fdt, node, "/cpu@1");
+//     fdt_appendprop_string(fdt, node1, "device_type", "cpu");
+//     fdt_appendprop_string(fdt, node1, "compatible", "hx,v1");
+//     fdt_appendprop_cell(fdt, node1, "reg", 1);
 
-    /* Timer */
-    node = fdt_add_subnode(fdt, 0, "/timer");
-    fdt_appendprop_string(fdt, node, "name", "timer");
-    fdt_appendprop_string(fdt, node, "device_type", "timer");
-    fdt_appendprop_string(fdt, node, "compatible", "arm,armv8-timer");
-    fdt_appendprop_cell(fdt, node, "interrupts", 0x00000001);
-    fdt_appendprop_cell(fdt, node, "interrupts", 0x00000001);
-    fdt_appendprop_cell(fdt, node, "interrupts", 0x00000f08);
-    fdt_appendprop_cell(fdt, node, "interrupts", 0x00000001);
-    fdt_appendprop_cell(fdt, node, "interrupts", 0x00000000);
-    fdt_appendprop_cell(fdt, node, "interrupts", 0x00000f08);
+//     /* refclk */
+//     node = fdt_add_subnode(fdt, 0, "/refclk24mhz");
+//     fdt_appendprop_string(fdt, node, "compatible", "fixed-clock");
+//     fdt_appendprop_string(fdt, node, "clock-output-names", "refclk24mhz");
+//     fdt_appendprop_cell(fdt, node, "#clock-cells", 0x00000000);
+//     fdt_appendprop_cell(fdt, node, "clock-frequency", 0x016e3600);
+//     fdt_appendprop_cell(fdt, node, "phandle", 2);
 
-    /* SoC */
-    node = fdt_add_subnode(fdt, 0, "/soc");
-    fdt_appendprop_string(fdt, node, "compatible", "simple-bus");
-    fdt_appendprop_cell(fdt, node, "#address-cells", 0x2);
-    fdt_appendprop_cell(fdt, node, "#size-cells", 0x2);
-    fdt_appendprop(fdt, node, "ranges", "", 0);
+//     /* Timer */
+//     node = fdt_add_subnode(fdt, 0, "/timer");
+//     fdt_appendprop_string(fdt, node, "name", "timer");
+//     fdt_appendprop_string(fdt, node, "device_type", "timer");
+//     fdt_appendprop_string(fdt, node, "compatible", "arm,armv8-timer");
+//     fdt_appendprop_cell(fdt, node, "interrupts", 0x00000001);
+//     fdt_appendprop_cell(fdt, node, "interrupts", 0x00000001);
+//     fdt_appendprop_cell(fdt, node, "interrupts", 0x00000f08);
+//     fdt_appendprop_cell(fdt, node, "interrupts", 0x00000001);
+//     fdt_appendprop_cell(fdt, node, "interrupts", 0x00000000);
+//     fdt_appendprop_cell(fdt, node, "interrupts", 0x00000f08);
 
-    /* Interrupt controller: Apple AIC */
-    siprintf(fdt_nodename, "/interrupt-controller@%llx", (uint64_t)dt_get_u32_prop("aic", "reg") + gIOBase);
-    node1 = fdt_add_subnode(fdt, node, fdt_nodename);
-    fdt_appendprop_string(fdt, node1, "name", "interrupt_controller");
-    fdt_appendprop_string(fdt, node1, "device_type", "interrupt_controller");
-    fdt_appendprop_string(fdt, node1, "compatible", "hx,aic");
-    fdt_appendprop_cell(fdt, node1, "phandle", 0x1);
-    fdt_appendprop_cell(fdt, node1, "linux,phandle", 0x1);
-    fdt_appendprop_cell(fdt, node1, "#interrupt-cells", 0x3);
-    fdt_appendprop_addrrange(fdt, 0, node1, "reg",
-                             ((uint64_t)dt_get_u32_prop("aic", "reg")) + gIOBase,
-                             0x8000);
-    fdt_appendprop(fdt, node1, "interrupt-controller", "", 0);
+//     /* SoC */
+//     node = fdt_add_subnode(fdt, 0, "/soc");
+//     fdt_appendprop_string(fdt, node, "compatible", "simple-bus");
+//     fdt_appendprop_cell(fdt, node, "#address-cells", 0x2);
+//     fdt_appendprop_cell(fdt, node, "#size-cells", 0x2);
+//     fdt_appendprop(fdt, node, "ranges", "", 0);
 
-    /* UART */
-    siprintf(fdt_nodename, "/serial@%llx", (uint64_t)dt_get_u32_prop("uart0", "reg") + gIOBase);
-    node1 = fdt_add_subnode(fdt, node, fdt_nodename);
-    fdt_appendprop_string(fdt, node1, "compatible", "hx,uart");
-    fdt_appendprop_addrrange(fdt, 0, node1, "reg",
-                             ((uint64_t)dt_get_u32_prop("uart0", "reg")) + gIOBase,
-                             0x4000);
-    fdt_appendprop_cell(fdt, node1, "interrupts", 0);
-    fdt_appendprop_cell(fdt, node1, "interrupts", dt_get_u32_prop("uart0", "interrupts"));
-    fdt_appendprop_cell(fdt, node1, "interrupts", 4);
-    fdt_appendprop_cell(fdt, node1, "clocks", 2);
-    fdt_appendprop_string(fdt, node1, "clock-names", "refclk");
+//     /* Interrupt controller: Apple AIC */
+//     siprintf(fdt_nodename, "/interrupt-controller@%llx", (uint64_t)dt_get_u32_prop("aic", "reg") + gIOBase);
 
-    /* DRAM */
-    node = fdt_add_subnode(fdt, 0, "/memory@800000000");
-    fdt_appendprop_addrrange(fdt, 0, node, "reg", 0x800000000, (gBootArgs->memSize - 0x02000000) & ~0x1FFFFFF);
-    fdt_appendprop_string(fdt, node, "device_type", "memory");
+//     node1 = fdt_add_subnode(fdt, node, fdt_nodename);
+//     fdt_appendprop_string(fdt, node1, "name", "interrupt_controller");
+//     fdt_appendprop_string(fdt, node1, "device_type", "interrupt_controller");
+//     fdt_appendprop_string(fdt, node1, "compatible", "apple,aic");
+//     fdt_appendprop_cell(fdt, node1, "phandle", 0x1);
+//     fdt_appendprop_cell(fdt, node1, "linux,phandle", 0x1);
+//     fdt_appendprop_cell(fdt, node1, "#interrupt-cells", 0x3);
+//     fdt_appendprop_addrrange(fdt, 0, node1, "reg",
+//                              ((uint64_t)dt_get_u32_prop("aic", "reg")) + gIOBase,
+//                              0x8000);
+//     fdt_appendprop(fdt, node1, "interrupt-controller", "", 0);
 
-    /* Reserved memory */
-    /*
-    node = fdt_add_subnode(fdt, 0, "/reserved-memory");
-    fdt_appendprop_cell(fdt, node, "#address-cells", 0x2);
-    fdt_appendprop_cell(fdt, node, "#size-cells", 0x2);
-    fdt_appendprop(fdt, node, "ranges", "", 0);
+//     /* UART */
+//     siprintf(fdt_nodename, "/serial@%llx", (uint64_t)dt_get_u32_prop("uart0", "reg") + gIOBase);
+//     node1 = fdt_add_subnode(fdt, node, fdt_nodename);
+//     fdt_appendprop_string(fdt, node1, "compatible", "hx,uart");
+//     fdt_appendprop_addrrange(fdt, 0, node1, "reg",
+//                              ((uint64_t)dt_get_u32_prop("uart0", "reg")) + gIOBase,
+//                              0x4000);
+//     fdt_appendprop_cell(fdt, node1, "interrupts", 0);
+//     fdt_appendprop_cell(fdt, node1, "interrupts", dt_get_u32_prop("uart0", "interrupts"));
+//     fdt_appendprop_cell(fdt, node1, "interrupts", 4);
+//     fdt_appendprop_cell(fdt, node1, "clocks", 2);
+//     fdt_appendprop_string(fdt, node1, "clock-names", "refclk");
 
-    uint64_t nomap_area = 0x800000000 + gBootArgs->memSize - 0x02000000;
-    siprintf(fdt_nodename, "/fw_area@%llx", nomap_area);
-    node1 = fdt_add_subnode(fdt, node, fdt_nodename);
-    fdt_appendprop_addrrange(fdt, 0, node1, "reg", nomap_area, 0x04000000);
-    fdt_appendprop(fdt, node1, "no-map", "", 0);
-*/
+//     /* DRAM */
+//     node = fdt_add_subnode(fdt, 0, "/memory@803000000");
+//     fdt_appendprop_addrrange(fdt, 0, node, "reg", 0x803000000, 0x8000000);
+//     fdt_appendprop_string(fdt, node, "device_type", "memory");
+
+//     /* Framebuffer */
+//     node = fdt_add_subnode(fdt, 0, "/memory@0x87d2e2000");
+//     fdt_appendprop_addrrange(fdt, 0, node, "reg", 0x87d2e2000, 2048 * 1536 * 4);
+//     fdt_appendprop_string(fdt, node, "device_type", "memory");
+
+// /* Reserved memory */
+//     node = fdt_add_subnode(fdt, 0, "/reserved-memory");
+//     fdt_appendprop_cell(fdt, node, "#address-cells", 0x2);
+//     fdt_appendprop_cell(fdt, node, "#size-cells", 0x2);
+//     fdt_appendprop(fdt, node, "ranges", "", 0);
+
+//     siprintf(fdt_nodename, "/ramoops@8fc00000");
+//     node1 = fdt_add_subnode(fdt, node, fdt_nodename);
+//     fdt_appendprop_string(fdt, node1, "compatible", "ramoops");
+//     fdt_appendprop_addrrange(fdt, 0, node1, "reg", 0x804000000, 0x1000000);
+//     fdt_appendprop_cell(fdt, node, "record-size", 0x10000);
+//     fdt_appendprop_cell(fdt, node, "console-size", 0x60000);
+//     fdt_appendprop_cell(fdt, node, "ftrace-size", 0x10000);
+//     fdt_appendprop_cell(fdt, node, "pmsg-size", 0x20000);
+//     fdt_appendprop_cell(fdt, node, "ecc-size", 16);
+
+//     siprintf(fdt_nodename, "/memory@%llx", 0x87d2e2000);
+//     node1 = fdt_add_subnode(fdt, node, fdt_nodename);
+//     fdt_appendprop_addrrange(fdt, 0, node1, "reg", 0x87d2e2000, 2048 * 1536 * 4);
+//     fdt_appendprop(fdt, node1, "no-map", "", 0);
 }
 
 void linux_dtree_late(void)
@@ -191,16 +205,6 @@ void linux_dtree_overlay(char *boot_args)
 {
     char fdt_nodename[64];
     int node = 0, node1 = 0;
-    siprintf(fdt_nodename, "/framebuffer@%lx", gBootArgs->Video.v_baseAddr);
-    node1 = fdt_add_subnode(fdt, node, fdt_nodename);
-    fdt_appendprop_addrrange(fdt, 0, node1, "reg", gBootArgs->Video.v_baseAddr, gBootArgs->Video.v_height * gBootArgs->Video.v_rowBytes);
-    fdt_appendprop_cell(fdt, node1, "width", gBootArgs->Video.v_width);
-    fdt_appendprop_cell(fdt, node1, "height", gBootArgs->Video.v_height);
-    fdt_appendprop_cell(fdt, node1, "stride", gBootArgs->Video.v_rowBytes);
-    fdt_appendprop_string(fdt, node1, "format", "a8b8g8r8");
-    fdt_appendprop_string(fdt, node1, "status", "okay");
-    fdt_appendprop_string(fdt, node1, "compatible", "simple-framebuffer");
-
     node = fdt_path_offset(fdt, "/chosen");
     if (node < 0)
     {
@@ -274,7 +278,7 @@ void fdt_select_dtree(void *fdt)
         len = (len << 8) | ebuf[2];
         len = (len << 8) | ebuf[3];
         ebuf += 4;
-        if (!strcmp(key, (char *)buf))
+        if (strcmp(key, (char *)buf))
         {
             iprintf("Found device tree for %s (%d bytes).\n", key, len);
             memmove(fdt, ebuf, len);
@@ -319,7 +323,6 @@ void linux_prep_boot()
         if (fdt_open_into((void *)fdt, (void *)fdt, LINUX_DTREE_SIZE))
         {
             iprintf("failed to apply overlay to fdt\n");
-            return;
         }
 
         iprintf("Kernel command line: %s\n", gLinuxCmdLine);
@@ -342,7 +345,7 @@ void linux_prep_boot()
     *colormatrix_mul_33 = 4095;
     puts("This is only supported on iPhone 7 for now and works to a lesser extent on other A10 devices. Behavior on non-A10 devices is undefined!!");
 
-    gEntryPoint = (void *)(0x800080000);
+    gEntryPoint = (void *)(0x803000000);
     uint64_t image_size = loader_xfer_recv_count;
     gLinuxStage = (void *)alloc_contig(image_size + LINUX_DTREE_SIZE);
     size_t dest_size = 0x10000000;
